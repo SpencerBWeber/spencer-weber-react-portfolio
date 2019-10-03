92364395a6ba2d52a08b163c386f273a62bcbc1e
JingleDjango95 <spencerweber019@gmail.com>
2019-10-02 19:31:14 -0600

    put container and item inside a portfolio folder

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react"
+
+import PortfolioItem from "./portfolio-item"
+
+export default class PortfolioContainer extends Component {
+	constructor() {
+		super()
+
+		console.log("Porfolio container as rendered")
+	}
+
+	PortfolioItems() {
+		const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"]
+
+		return data.map(item => {
+			return <PortfolioItem title={item} url="google.com"/>
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Portfolio items go here updates...</h2>
+
+				{this.PortfolioItems()}
+			</div>
+		)
+	}
+}
