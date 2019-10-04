da72deb5ac3a1ad4a4370d988013dbde14c7380c
JingleDjango95 <spencerweber019@gmail.com>
2019-10-04 09:51:12 -0600

    incorperated Switch/Route methods

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
-import PortfolioContainer from "./portfolio/portfolio-container";
+import PortfolioComponent from "./portfolio/portfolio-container";
 import NavigationComponent from "./navigation/navigation-container"
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <NavigationComponent />
+
+
+        <Router>
+          <div>
+            <NavigationComponent />
+
+            <Switch>
+              <Route exact path="/" component='' />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Spencer Weber's Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
+        <PortfolioComponent />
       </div>
     );
   }
---
+++ src/components/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div className="">
+      
+    </div>
+  )
+}
+
+export default about
