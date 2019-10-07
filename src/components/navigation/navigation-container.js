b583f2a8af91747a70c555de5df556157544c5e4
JingleDjango95 <spencerweber019@gmail.com>
2019-10-07 13:33:58 -0600

    added nav-bar styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
   constructor() {
-    super()
+    super();
   }
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
-        <NavLink to="/about-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
-        <NavLink to="/contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
-        {false ? <button>Add Blog</button> : null }
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+          {false ? <button>Add Blog</button> : null}
+        </div>
+        <div className="right-side">SPENCER WEBER</div>
       </div>
-    )
+    );
   }
 }
