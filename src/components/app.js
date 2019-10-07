60c2125728ab168664da6d99717d440962f04c3c
JingleDjango95 <spencerweber019@gmail.com>
2019-10-07 14:14:26 -0600

    cleaned up app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import moment from "moment";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
-import NavigationComponent from "./navigation/navigation-container"
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
-import Blog from './pages/blog'
-import PortfolioDetail from './portfolio/portfolio-detail'
-import NoMatch from "./pages/no-match"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavigationComponent from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -19,8 +14,6 @@ export default class App extends Component {
       <div className="app">
         <Router>
           <div>
-            <h1>Spencer Weber's Portfolio</h1>
-            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationComponent />
 
             <Switch>
@@ -28,8 +21,12 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              <Route exact path="/portfolio/:slug" component={PortfolioDetail}/>
-              <Route component={NoMatch}/>
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
