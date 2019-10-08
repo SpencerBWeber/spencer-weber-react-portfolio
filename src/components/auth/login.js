0581d295a16805c7e38f240dbbca28474a18de23
JingleDjango95 <spencerweber019@gmail.com>
2019-10-08 14:04:51 -0600

    setup auth event listeners

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    console.log(event);
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="">
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-        <form>
-          <input type="text" />
-          <input type="password" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
         </form>
       </div>
     );
