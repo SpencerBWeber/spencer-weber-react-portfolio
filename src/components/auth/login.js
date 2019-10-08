11351992a0b0fe25ffd583d0fc3b40dd8810370a
JingleDjango95 <spencerweber019@gmail.com>
2019-10-08 14:29:03 -0600

    created server-side validation for client

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,7 +7,8 @@ export default class Login extends Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      errorText: ""
     };
   }
 
@@ -24,14 +25,26 @@ export default class Login extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        console.log("response", response);
+        if (response.data.status === "created") {
+          console.log("you can come in");
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          });
+        }
+      })
+      .catch(error => {
+        this.setState({
+          errorText: "An error occurred"
+        });
       });
     event.preventDefault();
   };
 
   handleChange = event => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      errorText: ""
     });
   };
 
@@ -39,6 +52,9 @@ export default class Login extends Component {
     return (
       <div className="">
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
+
         <form onSubmit={this.handleSubmit}>
           <input
             type="email"
