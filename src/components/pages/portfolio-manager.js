78728cb5ee8c5b2ce67f5c77c3393b8ec6887a92
JingleDjango95 <spencerweber019@gmail.com>
2019-10-09 13:31:23 -0600

    created portfolio manager scss file

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,9 +3,13 @@ import React from "react";
 export default class PortfolioManager extends React.Component {
   render() {
     return (
-      <div className="">
-        <h1>PortfolioManager</h1>
-        <h1>PortfolioManager</h1>
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio Form...</h1>
+        </div>
+        <div className="right-column">
+          <h1>PortfolioManager</h1>
+        </div>
       </div>
     );
   }
