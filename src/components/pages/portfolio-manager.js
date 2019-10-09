f341d8627ec9ddef2569da0ecaf074c9d406ab8b
JingleDjango95 <spencerweber019@gmail.com>
2019-10-09 13:27:20 -0600

    created portfolio manager page for admin

---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class PortfolioManager extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <h1>PortfolioManager</h1>
+        <h1>PortfolioManager</h1>
+      </div>
+    );
+  }
+}
