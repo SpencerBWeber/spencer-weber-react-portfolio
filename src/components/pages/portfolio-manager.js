cae469e8f07318b6faaaa7b832b04af8f4db9596
JingleDjango95 <spencerweber019@gmail.com>
2019-10-09 13:53:05 -0600

    made contact with backend api

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default class PortfolioManager extends React.Component {
+export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jingledjango.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
