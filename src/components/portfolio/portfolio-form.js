ce4ea6584e619fddce3e1aebfce8a3d0db05066e
JingleDjango95 <spencerweber019@gmail.com>
2019-10-11 15:41:02 -0600

    updated form styles with mixins

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -122,7 +122,7 @@ export default class PortfolioForm extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
-        <div>
+        <div className="two-column">
           <input
             type="text"
             name="name"
@@ -139,7 +139,7 @@ export default class PortfolioForm extends Component {
             onChange={this.handleChange}
           />
         </div>
-        <div>
+        <div className="two-column">
           <input
             type="text"
             name="position"
@@ -159,7 +159,7 @@ export default class PortfolioForm extends Component {
             <option value="Enterprise">Enterprise</option>
           </select>
         </div>
-        <div>
+        <div className="one-column">
           <textarea
             type="text"
             name="description"
@@ -169,7 +169,7 @@ export default class PortfolioForm extends Component {
           />
         </div>
 
-        <div className="image-uploaders">
+        <div className="image-uploaders three-column">
           <DropzoneComponent
             ref={this.thumbRef}
             config={this.componentConfig()}
