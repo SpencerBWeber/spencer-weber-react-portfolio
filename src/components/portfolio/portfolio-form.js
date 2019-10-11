c811272980d7ee5575aefde35fc447b087d1469b
JingleDjango95 <spencerweber019@gmail.com>
2019-10-11 15:53:07 -0600

    finished form styles

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -175,23 +175,31 @@ export default class PortfolioForm extends Component {
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleThumbDrop()}
-          />
+          >
+            <div className="dz-message">Thumbnail</div>
+          </DropzoneComponent>
           <DropzoneComponent
             ref={this.bannerRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleBannerDrop()}
-          />
+          >
+            <div className="dz-message">Banner</div>
+          </DropzoneComponent>
           <DropzoneComponent
             ref={this.logoRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleLogoDrop()}
-          />
+          >
+            <div className="dz-message">Logo</div>
+          </DropzoneComponent>
         </div>
 
         <div>
-          <button type="submit">Save</button>
+          <button className="btn" type="submit">
+            Save
+          </button>
         </div>
       </form>
     );
