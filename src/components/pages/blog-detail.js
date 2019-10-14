2f0cf8df63bb2f120d0aef861a0c09293d087b51
JingleDjango95 <spencerweber019@gmail.com>
2019-10-14 17:46:44 -0600

    made blog detail api call for blogId

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
+
+  getBlogItem = () => {
+    axios
+      .get(
+        `https://jingledjango.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+      )
+      .then(response => {
+        console.log("getBlogItem response", response);
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  };
+
+  componentDidMount = () => {
+    this.getBlogItem();
+  };
+
   render() {
-    return <div className=""></div>;
+    return (
+      <div>
+        <h1>Blog Detail</h1>
+      </div>
+    );
   }
 }
