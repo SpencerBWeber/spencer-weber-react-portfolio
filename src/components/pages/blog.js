8c9f66f2cecf27bd1e110cb26c326f899f17588d
JingleDjango95 <spencerweber019@gmail.com>
2019-10-14 17:13:07 -0600

    called blog api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: []
+    };
   }
+
+  getBlogItems = () => {
+    axios
+      .get("https://jingledjango.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  };
+
+  componentWillMount = () => {
+    this.getBlogItems();
+  };
 
   render() {
     return (
