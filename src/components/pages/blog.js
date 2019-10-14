32a7ae8519fe2fd11bd8b4d58377878298142e06
JingleDjango95 <spencerweber019@gmail.com>
2019-10-14 17:28:48 -0600

    created blog item functional component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
@@ -32,7 +33,7 @@ class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <h1>{blogItem.title}</h1>;
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
 
     return <div>{blogRecords}</div>;
