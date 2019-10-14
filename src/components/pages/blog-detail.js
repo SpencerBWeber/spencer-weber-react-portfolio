b0b90aea9084ba5a22a0543223a02c36c0a30c7e
JingleDjango95 <spencerweber019@gmail.com>
2019-10-14 17:53:40 -0600

    rendered blog data

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -17,7 +17,9 @@ export default class BlogDetail extends Component {
         `https://jingledjango.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
       )
       .then(response => {
-        console.log("getBlogItem response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog
+        });
       })
       .catch(error => {
         console.log("getBlogItem error", error);
@@ -29,9 +31,18 @@ export default class BlogDetail extends Component {
   };
 
   render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
     return (
       <div>
-        <h1>Blog Detail</h1>
+        <h1>{title}</h1>
+        <img src={featured_image_url} />
+        <div>{content}</div>
       </div>
     );
   }
