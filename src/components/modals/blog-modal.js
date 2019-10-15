a63e67804e07cc2f9f4026be5b4bc1e12d664cdf
JingleDjango95 <spencerweber019@gmail.com>
2019-10-14 20:10:15 -0600

    created initial modal form component

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
+import BlogForm from "../blog/blog-form";
 
 ReactModal.setAppElement(".app-wrapper");
 
@@ -31,7 +32,7 @@ export default class BlogModal extends Component {
         }}
         isOpen={this.props.modalIsOpen}
       >
-        <h1>I'm in a modal</h1>
+        <BlogForm />
       </ReactModal>
     );
   }
