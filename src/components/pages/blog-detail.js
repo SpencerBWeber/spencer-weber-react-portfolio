269c28e0dfd423c73cb9e41d79ccee2e3b692b5e
JingleDjango95 <spencerweber019@gmail.com>
2019-10-15 14:45:50 -0600

    created a featured image functional component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+
+import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
   constructor(props) {
@@ -43,11 +46,9 @@ export default class BlogDetail extends Component {
         <div className="content-container">
           <h1>{title}</h1>
 
-          <div className="featured-image-wrapper">
-            <img src={featured_image_url} />
-          </div>
+          <BlogFeaturedImage img={this.featured_image_url} />
 
-          <div className="content">{content}</div>
+          <div className="content">{ReactHtmlParser(content)}</div>
         </div>
       </div>
     );
