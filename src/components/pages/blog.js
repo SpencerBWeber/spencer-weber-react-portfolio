3bd29725f7090de1bec5a503ee255f0808a262b4
JingleDjango95 <spencerweber019@gmail.com>
2019-10-14 18:41:03 -0600

    setup page count for infinite scroll

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,7 +8,9 @@ class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
     };
 
     this.activateInfiniteScroll();
@@ -26,13 +28,18 @@ class Blog extends Component {
   };
 
   getBlogItems = () => {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+
     axios
       .get("https://jingledjango.devcamp.space/portfolio/portfolio_blogs", {
         withCredentials: true
       })
       .then(response => {
         this.setState({
-          blogItems: response.data.portfolio_blogs
+          blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records
         });
       })
       .catch(error => {
