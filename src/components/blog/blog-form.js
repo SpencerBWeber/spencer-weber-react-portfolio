8ce4e2b2b7369c52a91b1a9b47f490fa6cf2ec3d
JingleDjango95 <spencerweber019@gmail.com>
2019-10-15 14:27:29 -0600

    fixed unmounted component warning

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -37,12 +37,13 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
-
         this.setState({
           title: "",
-          blog_status: ""
+          blog_status: "",
+          content: ""
         });
+
+        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
       })
       .catch(error => {
         console.log("handleSubmit for blog error", error);
