c3b2949cfd0a92a73a70c1639f861d078f1bbc45
JingleDjango95 <spencerweber019@gmail.com>
2019-10-15 09:27:46 -0600

    created initial editor tools for blog, and added styles

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import RichTextEditor from "../forms/rich-text-editor";
+
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
@@ -66,6 +68,10 @@ export default class BlogForm extends Component {
             placeholder="Blog Status"
             value={this.state.blog_status}
           />
+        </div>
+
+        <div className="one-column">
+          <RichTextEditor />
         </div>
 
         <button className="btn">Save</button>
