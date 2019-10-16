2b8d92ff602832df8ffb8c9145c3215377323ff0
JingleDjango95 <spencerweber019@gmail.com>
2019-10-15 19:26:22 -0600

    initial portfolio detail item rendering

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,6 +4,10 @@ import axios from "axios";
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      portfolioItem: {}
+    };
   }
 
   componentWillMount = () => {
@@ -17,7 +21,9 @@ export default class PortfolioDetail extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        console.log("getPortfolioItem response", response);
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        });
       })
       .catch(error => {
         console.log("getPortfolioItem error", error);
@@ -25,9 +31,20 @@ export default class PortfolioDetail extends Component {
   };
 
   render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url
+    } = this.state.portfolioItem;
+
     return (
       <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
     );
   }
