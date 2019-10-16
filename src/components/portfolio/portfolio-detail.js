33ceb05548b3d793bd6e6f02d3366ba0e639ad5c
JingleDjango95 <spencerweber019@gmail.com>
2019-10-15 19:18:32 -0600

    built initial portfolio detail page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
-const PortfolioDetail = props => {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  )
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount = () => {
+    this.getPortfolioItem();
+  };
+
+  getPortfolioItem = () => {
+    axios
+      .get(
+        `https://jingledjango.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("getPortfolioItem response", response);
+      })
+      .catch(error => {
+        console.log("getPortfolioItem error", error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
 }
-
-export default PortfolioDetail
