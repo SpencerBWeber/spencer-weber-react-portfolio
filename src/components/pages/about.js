7a79e2e303fc725b13994e349f8fad0c473eed65
JingleDjango95 <spencerweber019@gmail.com>
2019-10-16 20:25:05 -0600

    updated about me

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -14,28 +14,20 @@ const About = () => {
       />
 
       <div className="right-column">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Sed blandit libero
-        volutpat sed cras ornare arcu. Faucibus vitae aliquet nec ullamcorper
-        sit amet risus nullam eget. Sed risus ultricies tristique nulla aliquet.
-        Habitant morbi tristique senectus et netus. Proin fermentum leo vel orci
-        porta non pulvinar. Mi bibendum neque egestas congue quisque egestas
-        diam in arcu. Augue mauris augue neque gravida in fermentum et. Vivamus
-        arcu felis bibendum ut tristique et egestas quis ipsum. Dui id ornare
-        arcu odio ut sem nulla pharetra. A arcu cursus vitae congue. Sit amet
-        facilisis magna etiam tempor. Arcu non sodales neque sodales ut. Ac
-        tincidunt vitae semper quis lectus nulla. Lacus vel facilisis volutpat
-        est velit egestas dui id ornare. Tristique et egestas quis ipsum
-        suspendisse. Porttitor rhoncus dolor purus non enim. Elementum nibh
-        tellus molestie nunc. Nisl rhoncus mattis rhoncus urna neque viverra.
-        Proin sed libero enim sed faucibus turpis in. Luctus venenatis lectus
-        magna fringilla urna porttitor rhoncus. Tempus quam pellentesque nec nam
-        aliquam sem et tortor. Tempor id eu nisl nunc mi ipsum. Interdum
-        consectetur libero id faucibus nisl tincidunt eget. Mattis rhoncus urna
-        neque viverra justo. Vitae tortor condimentum lacinia quis vel eros
-        donec. Sed viverra tellus in hac. Ut aliquam purus sit amet. Fermentum
-        posuere urna nec tincidunt praesent semper feugiat nibh. A condimentum
-        vitae sapien pellentesque habitant morbi tristique.
+        <p>
+          I've always sought out opportunities to challenge and expand my
+          knowledge. Although my career path has taken many twists and turns -
+          from ensuring quality food as a cook, guaranteeing excellent customer
+          satisfaction as a Department Manager, to programming plasma and
+          acetylene CAD machines - I've always engaged my passions to help
+          others and solve problems.
+        </p>
+        <p>
+          As a software engineer, I enjoy using my obsessive attention to
+          detail, my unmatched love for creating things, and my mission-driven
+          work ethic to literally change the world. That's why I'm thrilled to
+          help build the technologies of tomorrow.
+        </p>
       </div>
     </div>
   );
